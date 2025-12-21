f3bf43b81e9e50d39d072fb070cb0655913a8f12
ackvanity <108710608+ackvanity@users.noreply.github.com>
2025-12-21 15:20:14 +0700

    deploy

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+export DEPLOYPATH=/home/hepivasi/public_html/
+/bin/cp *.html $DEPLOYPATH
+/bin/cp *.css $DEPLOYPATH
+/bin/cp *.md $DEPLOYPATH
+/bin/cp *.png $DEPLOYPATH
+/bin/cp *.jpg $DEPLOYPATH
+/bin/cp *.mjs $DEPLOYPATH
+/bin/cp *.html $DEPLOYPATH
+/bin/cp *.js $DEPLOYPATH
+/bin/cp -r blog $DEPLOYPATH/blog
+/bin/cp -r media $DEPLOYPATH/media
+/bin/cp -r projects $DEPLOYPATH/projects
