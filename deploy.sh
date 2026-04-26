380c4c509865f0aeccc5ce05441b708c5b48e56e
ackvanity <108710608+ackvanity@users.noreply.github.com>
2026-04-26 18:47:58 +0700

    fix

--- deploy.sh
+++ deploy.sh
@@ -15,7 +15,7 @@ export DEPLOYPATH=/home/hepivasi/public_html/
 /bin/cp -r media $DEPLOYPATH/media
 /bin/cp -r projects $DEPLOYPATH/projects
 
-chmod +x $$DEPLOYPATH/minify
+chmod +x $DEPLOYPATH/minify
 $DEPLOYPATH/minify -i --recursive $DEPLOYPATH > $DEPLOYPATH/log.txt
 
 echo "Success" > $DEPLOYPATH/deploy.txt
